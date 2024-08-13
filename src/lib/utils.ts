651afc7ea5f6c7d4206d11ed7b78b0e56aa1f46e
abdullahqudeer <abdullahqudeer@gmail.com>
2024-08-14 04:14:43 +0500

    Added accounts icon

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,19 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-// Renders conditional classes and merge tailwind classes in js without style conflicts.
+/**
+ * Combines multiple class names or class arrays into a single class name string.
+ * @param {...ClassValue[]} inputs - The class names or class arrays to combine.
+ * @returns A single string containing all the combined class names.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// generates array of numbers
+/**
+ * Generates an array of numbers
+ * @param {number} count - The number of elements to generate in the array.
+ * @returns {number[]} An array of numbers.
+ */
 export const arrayNumberGenerator = (count: number) =>
   Array.from(Array(count).keys());
