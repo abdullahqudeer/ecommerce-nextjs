7817d8c995b6019365c3393b1dff68491d318d25
abdullahqudeer <abdullahqudeer@gmail.com>
2024-09-03 20:15:00 +0530

    implement update cart

--- src/components/NumberInput/NumberInput.tsx
+++ src/components/NumberInput/NumberInput.tsx
@@ -1,24 +1,21 @@
 'use client';
 
 import { cn } from '@/lib/utils';
-import { FC, useEffect, useState } from 'react';
+import { FC } from 'react';
 
 interface NumberInputProps {
   className?: string;
   value?: number;
   inputClass?: string;
+  onChange?: (type: string) => void;
 }
 
 const NumberInput: FC<NumberInputProps> = ({
   className,
   value,
   inputClass,
+  onChange
 }) => {
-  const [inputValue, setValue] = useState(0);
-
-  useEffect(() => {
-    value && setValue(value);
-  }, [value]);
 
   return (
     <div
@@ -27,26 +24,27 @@ const NumberInput: FC<NumberInputProps> = ({
         className
       )}
     >
-      <div
+      
+      <button
         className="absolute flex w-[26px] items-center justify-center left-0 top-0 bottom-0 [z-1] cursor-pointer"
-        onClick={() => setValue(inputValue > 1 ? inputValue - 1 : inputValue)}
+        onClick={() => onChange && onChange("decrement")}
       >
         <i className="las la-minus text-[10px] text-black-75"></i>
-      </div>
+      </button>
       <input
         className={cn(
           'bg-transparent border-0 h-10 w-[131px] px-[26px] text-center text-black-500 text-sm font-extralight !outline-none',
           inputClass
         )}
-        value={inputValue}
-        onChange={(e: any) => setValue(e.target.value)}
+        value={value || 0}
+        readOnly
       />
-      <div
+      <button
         className="absolute flex w-[26px] items-center justify-center right-0 top-0 bottom-0 z-[1] cursor-pointer outline-0"
-        onClick={() => setValue(inputValue + 1)}
+        onClick={() => onChange && onChange("increment")}
       >
         <i className="las la-plus  text-[10px] text-black-75"></i>
-      </div>
+      </button>
     </div>
   );
 };
