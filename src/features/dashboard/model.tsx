b6d2150a9cd1f974d8f27d46a6dfb7c6c5f94010
abdullahqudeer <abdullahqudeer@gmail.com>
2024-09-06 18:01:11 +0530

    address apis added

---
+++ src/features/dashboard/model.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+
+const Modal = ({ onClose, children }:any) => {
+  // Close modal when pressing the Escape key
+  useEffect(() => {
+    const handleEsc = (event :any) => {
+      if (event.keyCode === 27) {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleEsc);
+    return () => window.removeEventListener('keydown', handleEsc);
+  }, [onClose]);
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="bg-white p-5 rounded-md shadow-lg max-w-lg w-full">
+        <div className="flex justify-between items-center mb-4">
+          <h3 className="text-xl font-semibold">Address</h3>
+          <button onClick={onClose} className="text-gray-600 hover:text-gray-800">
+            &times;
+          </button>
+        </div>
+        {/* Modal content */}
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
---
+++ src/store/slices/shippingaddress/shippingAddressSlice.ts
@@ -0,0 +1,50 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import type { RootState } from '@/store';
+
+
+export interface ShippingAddressSlice {
+  user_id: number
+  first_name: string
+  last_name: string
+  phone: number
+  email: string
+  address_line1: string
+  address_line2: string
+  city: string
+  state: string
+  country: string
+  postal_code: number
+}
+
+const initialState: ShippingAddressSlice = {
+  user_id: 0,
+  first_name: "",
+  last_name: "",
+  phone: 0,
+  email: "",
+  address_line1: "",
+  address_line2: "",
+  city: "",
+  state: "",
+  country: "",
+  postal_code: 0
+}
+
+export const siteShippingAddressSlice = createSlice({
+  name: 'shippingaddress',
+  initialState,
+  reducers: {
+    updateShippingAddress: (state, action: PayloadAction<Partial<ShippingAddressSlice>>) => {
+      Object.assign(state, action.payload);
+    }
+  },
+});
+
+export const {
+  updateShippingAddress
+} = siteShippingAddressSlice.actions;
+
+// Selectors
+export const selectShippingAddress = (state: RootState) => state.siteShippingAddressSlice;
+
+export default siteShippingAddressSlice.reducer;
