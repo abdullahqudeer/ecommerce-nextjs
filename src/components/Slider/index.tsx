740b0012c1d10fb9cc9685e7d796ef15976de07d
abdullahqudeer <abdullahqudeer@gmail.com>
2024-09-17 21:42:58 +0500

    image in slider insert by next image tag

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { SwiperSlide } from 'swiper/react';
+import Image from 'next/image';
 
 import SlideContent from './elements/SlideContent';
 import BaseSlider from './elements/BaseSlider';
@@ -9,41 +10,48 @@ import { useEffect, useState } from 'react';
 import { SlidersState } from '@/store/slices/sliders/slidersSlice';
 
 const Slider: React.FC = () => {
-  const [sliders, setSliders] = useState([])
-  const [fetchSliders] = useFetchSlidersMutation()
+  const [sliders, setSliders] = useState<SlidersState[]>([]);
+
+  const [fetchSliders] = useFetchSlidersMutation();
 
   const getSlidersHandler = async () => {
     try {
       let { data } = await fetchSliders({}).unwrap();
-
-      setSliders(data)
-
+      setSliders(data);
     } catch (error) {
+      // handle error
     }
-  }
+  };
 
   useEffect(() => {
-    getSlidersHandler()
-  }, [])
+    getSlidersHandler();
+  }, []);
 
   return (
     <BaseSlider className='h-[460px] lg:h-[480px] xl:h-[560px]'>
       {
-        sliders.map((el: SlidersState, index) => {
-          return <SwiperSlide key={index} style={{ backgroundImage: `url('${el.image}')` }} className={`bg-cover bg-center`}>
-            <SlideContent
-              title={el.sub_title}
-              heading={
-                <>
-                  {el.title}
-                </>
-              }
-              headingClass={`!text-${el.title_color}`}
-              btnText={el.button_text}
-              btnLink={el.link}
-            />
+        sliders.map((el: SlidersState, index) => (
+          <SwiperSlide key={index} className='relative'>
+            <div className='absolute inset-0 z-0'>
+              <Image 
+                src={el.image} 
+                alt={el.title} 
+                fill
+                style={{ objectFit: 'cover' }} 
+                priority={true} 
+              />
+            </div>
+            <div className='relative top-52 z-10'>
+              <SlideContent
+                title={el.sub_title}
+                heading={<>{el.title}</>}
+                headingClass={`!text-${el.title_color}`}
+                btnText={el.button_text}
+                btnLink={el.link}
+              />
+            </div>
           </SwiperSlide>
-        })
+        ))
       }
     </BaseSlider>
   );
