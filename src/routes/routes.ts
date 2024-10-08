e020ca6872df0f899df66852259faf4006f4aef7
abdullahqudeer <abdullahqudeer@gmail.com>
2024-10-08 22:14:42 +0500

    added protected routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,15 +2,23 @@ const routes = {
     home: "/",
     auth: "/auth",
     about: "/about",
+    cart: "/cart",
+    checkout: "/checkout",
+    whishlist: "/whishlist",
+    // 
     products: "/products",
     productDetails: (slug: string) => `/products/${slug}`,
+    // 
     account: "/account",
     orders: "/account/orders",
     addresses: "/account/addresses",
     accountDetails: "/account/account-details"
+
 };
 // these are the routes user can only access when he is logged in
 export const protectedRoutes = [
     routes.account,
-]
+    routes.cart,
+    routes.checkout,
+    routes.whishlist]
 export default routes;
